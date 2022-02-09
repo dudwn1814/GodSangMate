b560433ddb1f2450a44ffb9d4268e2166a558c17
AYJ <dudwn1814@naver.com>
2022-02-09 17:16:35 +0900

    Add To Do List Page buttons and textViews

--- app/src/main/java/com/example/re_todolist/MainActivity.java
+++ app/src/main/java/com/example/re_todolist/MainActivity.java
@@ -2,13 +2,21 @@ package com.example.re_todolist;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.PopupMenu;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.dinuscxj.progressbar.CircleProgressBar;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements CircleProgressBar.ProgressFormatter {
+
+    private static final String DEFAULT_PATTERN = "%d%%";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +31,53 @@ public class MainActivity extends AppCompatActivity {
                 MainActivity.this.startActivity(calendarIntent);
             }
         });
+
+        Button optionBtn = findViewById(R.id.optionBtn);
+        optionBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PopupMenu p = new PopupMenu(getApplicationContext(), view);
+                getMenuInflater().inflate(R.menu.menu_main, p.getMenu());
+
+                p.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        Toast.makeText(getApplicationContext(),
+                                "팝업메뉴 이벤트 처리 - "
+                                        + item.getTitle(),
+                                Toast.LENGTH_SHORT).show();
+                        return false;
+                    }
+                });
+                p.show();
+            }
+        });
+
+        CircleProgressBar circleProgressBar_group = findViewById(R.id.circlebar_group);
+        CircleProgressBar circleProgressBar_personal = findViewById(R.id.circlebar_personal);
+
+        circleProgressBar_group.setProgress(50);
+        circleProgressBar_personal.setProgress(70);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == 1) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    public CharSequence format(int progress, int max) {
+        return String.format(DEFAULT_PATTERN, (int) ((float) progress / (float) max * 100));
+    }
 }
