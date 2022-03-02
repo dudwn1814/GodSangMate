9717d478274999c4c012614951eaa951885032b6
sujin <susuzinga@naver.com>
2022-03-03 07:20:12 +0900

    alarm (더 조져진..)

--- app/src/main/java/com/example/re_todolist/AlarmReceiver.java
+++ app/src/main/java/com/example/re_todolist/AlarmReceiver.java
@@ -3,13 +3,21 @@ package com.example.re_todolist;
 import static android.content.Context.MODE_PRIVATE;
 
 import android.app.Activity;
+import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Vibrator;
 import android.widget.Toast;
 
 import androidx.core.app.NotificationCompat;
@@ -20,26 +28,31 @@ import java.util.Date;
 import java.util.Locale;
 
 public class AlarmReceiver extends BroadcastReceiver {
+    private MediaPlayer mediaPlayer;
+
     @Override
     public void onReceive(Context context, Intent intent) {
+        Bundle bundle = intent.getExtras();
+        String activity = bundle.getString("activity");
 
         NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-        Intent notificationIntent = new Intent(context, MainActivity.class);
+//        Intent notificationIntent = new Intent(context, MainActivity.class);
+        Intent fullScreenIntent = new Intent(context, MainActivity.class);
 
-        notificationIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP
+        fullScreenIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP
                 | Intent.FLAG_ACTIVITY_SINGLE_TOP);
 
-        PendingIntent pendingI = PendingIntent.getActivity(context, 0,
-                notificationIntent, 0);
+//        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "default");
 
+        PendingIntent fullScreenPendingIntent = PendingIntent.getActivity(context, 0,
+                fullScreenIntent, 0);
 
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "default");
-
+        NotificationCompat.Builder full_builder = new NotificationCompat.Builder(context, "default");
 
         //OREO API 26 이상에서는 채널 필요
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
 
-            builder.setSmallIcon(R.drawable.ic_launcher_foreground); //mipmap 사용시 Oreo 이상에서 시스템 UI 에러남
+            full_builder.setSmallIcon(R.drawable.ic_launcher_foreground); //mipmap 사용시 Oreo 이상에서 시스템 UI 에러남
 
 
             String channelName ="매일 알람 채널";
@@ -53,23 +66,29 @@ public class AlarmReceiver extends BroadcastReceiver {
                 // 노티피케이션 채널을 시스템에 등록
                 notificationManager.createNotificationChannel(channel);
             }
-        }else builder.setSmallIcon(R.mipmap.ic_launcher); // Oreo 이하에서 mipmap 사용하지 않으면 Couldn't create icon: StatusBarIcon 에러남
+        }else full_builder.setSmallIcon(R.mipmap.ic_launcher); // Oreo 이하에서 mipmap 사용하지 않으면 Couldn't create icon: StatusBarIcon 에러남
 
 
-        builder.setAutoCancel(true)
-                .setDefaults(NotificationCompat.DEFAULT_ALL)
+        mediaPlayer = MediaPlayer.create(context, R.raw.alarm);
+        mediaPlayer.setLooping(true);
+        mediaPlayer.setAudioStreamType(AudioManager.STREAM_ALARM);
+
+        full_builder
+                .setAutoCancel(true)
+                .setVibrate(new long[]{0, 100, 1000})
+                .setDefaults(Notification.DEFAULT_LIGHTS)
+                .setSound(Uri. parse( "android.resource://com.example.re_todolist/raw/alarm"))
                 .setWhen(System.currentTimeMillis())
-
-                .setTicker("{Time to watch some cool stuff!}")
-                .setContentTitle("TODO를 실행할 시간입니다!")
-                .setContentText("실행")
-                .setContentInfo("실행 할 내용")
-                .setContentIntent(pendingI);
+                .setContentTitle("갓생메이트와 약속한 그룹 TODO")
+                .setContentText("<"+activity+">"+" 해야 할 시간입니다!")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setFullScreenIntent(fullScreenPendingIntent, true);
 
         if (notificationManager != null) {
 
             // 노티피케이션 동작시킴
-            notificationManager.notify(1234, builder.build());
+            notificationManager.notify(1234, full_builder.build());
+            mediaPlayer.start();
 
             Calendar nextNotifyTime = Calendar.getInstance();
 
@@ -81,9 +100,11 @@ public class AlarmReceiver extends BroadcastReceiver {
             editor.putLong("nextNotifyTime", nextNotifyTime.getTimeInMillis());
             editor.apply();
 
-            Date currentDateTime = nextNotifyTime.getTime();
-            String date_text = new SimpleDateFormat("yyyy년 MM월 dd일 EE요일 a hh시 mm분 ", Locale.getDefault()).format(currentDateTime);
-            Toast.makeText(context.getApplicationContext(),"다음 알람은 " + date_text + "으로 알람이 설정되었습니다!", Toast.LENGTH_SHORT).show();
+//            Date currentDateTime = nextNotifyTime.getTime();
+//            String date_text = new SimpleDateFormat("yyyy년 MM월 dd일 EE요일 a hh시 mm분 ", Locale.getDefault()).format(currentDateTime);
+//            Toast.makeText(context.getApplicationContext(),"다음 알람은 " + date_text + "으로 알람이 설정되었습니다!", Toast.LENGTH_SHORT).show();
         }
+
+
     }
 }
--- app/src/main/java/com/example/re_todolist/CreateToDoActivity.java
+++ app/src/main/java/com/example/re_todolist/CreateToDoActivity.java
@@ -126,65 +126,28 @@ public class CreateToDoActivity extends AppCompatActivity {
         EditText todoText = findViewById(R.id.textInput);
 
         //알람 시간 입력
-        final TimePicker picker = (TimePicker) findViewById(R.id.timePicker);
+        TimePicker picker = (TimePicker) findViewById(R.id.timePicker);
         picker.setIs24HourView(true);
 
-
-        // 앞서 설정한 값으로 보여주기
-        // 없으면 디폴트 값은 현재시간
-        SharedPreferences sharedPreferences = getSharedPreferences("daily alarm", MODE_PRIVATE);
-        long millis = sharedPreferences.getLong("nextNotifyTime", Calendar.getInstance().getTimeInMillis());
-
-        Calendar nextNotifyTime = new GregorianCalendar();
-        nextNotifyTime.setTimeInMillis(millis);
-
-        Date nextDate = nextNotifyTime.getTime();
-        String date_text = new SimpleDateFormat("yyyy년 MM월 dd일 EE요일 a hh시 mm분 ", Locale.getDefault()).format(nextDate);
-        Toast.makeText(getApplicationContext(), "[처음 실행시] 다음 알람은 " + date_text + "으로 알람이 설정되었습니다!", Toast.LENGTH_SHORT).show();
-
-
-        // 이전 설정값으로 TimePicker 초기화
-        Date currentTime = nextNotifyTime.getTime();
-        SimpleDateFormat HourFormat = new SimpleDateFormat("kk", Locale.getDefault());
-        SimpleDateFormat MinuteFormat = new SimpleDateFormat("mm", Locale.getDefault());
-
-        int pre_hour = Integer.parseInt(HourFormat.format(currentTime));
-        int pre_minute = Integer.parseInt(MinuteFormat.format(currentTime));
-
-
-        if (Build.VERSION.SDK_INT >= 23) {
-            picker.setHour(pre_hour);
-            picker.setMinute(pre_minute);
-        } else {
-            picker.setCurrentHour(pre_hour);
-            picker.setCurrentMinute(pre_minute);
-        }
 
         //투두 저장
         Button saveButton = findViewById(R.id.saveButton);
         saveButton.setOnClickListener(view -> {
             activity = (todoText.getText().toString().isEmpty() ? "" : todoText.getText().toString());
-            int hour, hour_24, minute;
-            String am_pm;
+            int hour, minute;
             if (Build.VERSION.SDK_INT >= 23) {
-                hour_24 = picker.getHour();
+                hour = picker.getHour();
                 minute = picker.getMinute();
-            } else {
-                hour_24 = picker.getCurrentHour();
-                minute = picker.getCurrentMinute();
             }
-            if (hour_24 > 12) {
-                am_pm = "PM";
-                hour = hour_24 - 12;
-            } else {
-                hour = hour_24;
-                am_pm = "AM";
+            else {
+                hour = picker.getCurrentHour();
+                minute = picker.getCurrentMinute();
             }
 
             // 현재 지정된 시간으로 알람 시간 설정
             Calendar calendar = Calendar.getInstance();
             calendar.setTimeInMillis(System.currentTimeMillis());
-            calendar.set(Calendar.HOUR_OF_DAY, hour_24);
+            calendar.set(Calendar.HOUR_OF_DAY, hour);
             calendar.set(Calendar.MINUTE, minute);
             calendar.set(Calendar.SECOND, 0);
 
@@ -192,18 +155,11 @@ public class CreateToDoActivity extends AppCompatActivity {
             if (calendar.before(Calendar.getInstance())) {
                 calendar.add(Calendar.DATE, 1);
             }
-
             Date currentDateTime = calendar.getTime();
-            String datetext = new SimpleDateFormat("yyyy년 MM월 dd일 EE요일 a hh시 mm분 ", Locale.getDefault()).format(currentDateTime);
-            Toast.makeText(getApplicationContext(), datetext + "으로 알람이 설정되었습니다!", Toast.LENGTH_SHORT).show();
+            String date_text = new SimpleDateFormat("yyyy년 MM월 dd일 EE요일 a hh시 mm분 ", Locale.getDefault()).format(currentDateTime);
+            Toast.makeText(getApplicationContext(), date_text + "으로 알람이 설정되었습니다!", Toast.LENGTH_SHORT).show();
 
-            //  Preference에 설정한 값 저장
-            SharedPreferences.Editor editor = getSharedPreferences("daily alarm", MODE_PRIVATE).edit();
-            editor.putLong("nextNotifyTime", (long) calendar.getTimeInMillis());
-            editor.apply();
-
-            diaryNotification(calendar);
-            time = calendar.getTime().toString();
+            diaryNotification(calendar, repeat, alarm, activity);
 
             //투두 객체 생성
             ToDoPrac todoObj = new ToDoPrac();
@@ -212,14 +168,22 @@ public class CreateToDoActivity extends AppCompatActivity {
             todoObj.setUid(uid);
 
             //DB에 입력한 날짜 저장
+            SharedPreferences.Editor editor = getSharedPreferences(activity, MODE_PRIVATE).edit();
+            editor.putLong("알림 시간", (long) calendar.getTimeInMillis());
+            editor.apply();
+
             long now = System.currentTimeMillis();
+            long alarm_time = calendar.getTimeInMillis();
             Date date = new Date(now);
+            Date alarms = new Date(alarm_time);
             SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
             String writeDate = format.format(date);
+            SimpleDateFormat times = new SimpleDateFormat("hh:mm");
+            String time = times.format(alarms);
 
             if (group) {
                 todoObj.setAlarm(alarm);
-                if (alarm) todoObj.setTime(time + am_pm.toUpperCase());
+                if (alarm) todoObj.setTime(time);
                 TDId = mDbRef.child("ToDoList").child(groupCode).child(writeDate).child("Group").push().getKey();
                 todoObj.setTdid(TDId);
                 mDbRef.child("ToDoList").child(groupCode).child(writeDate).child("Group").child(TDId).setValue(todoObj);
@@ -235,46 +199,54 @@ public class CreateToDoActivity extends AppCompatActivity {
     }
 
 
-    void diaryNotification(Calendar calendar) {
+    void diaryNotification(Calendar calendar, boolean repeat, boolean alarm, String activity) {
 //        PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
 //        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
 //        Boolean dailyNotify = sharedPref.getBoolean(SettingsActivity.KEY_PREF_DAILY_NOTIFICATION, true);
-        boolean dailyNotify = true; // 무조건 알람을 사용
+        Boolean dailyNotify = alarm;
+        // 무조건 알람을 사용
 
         PackageManager pm = this.getPackageManager();
         ComponentName receiver = new ComponentName(this, DeviceBootReceiver.class);
         Intent alarmIntent = new Intent(this, AlarmReceiver.class);
+        alarmIntent.putExtra("activity",activity);
         PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 0, alarmIntent, 0);
         AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
 
 
         // 사용자가 매일 알람을 허용했다면
         if (dailyNotify) {
-
-            if (alarmManager != null) {
-
+            if (repeat) {
                 alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(),
                         AlarmManager.INTERVAL_DAY, pendingIntent);
 
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
                     alarmManager.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent);
                 }
+                Toast.makeText(getApplicationContext(),"반복 알림이 설정됐습니다",Toast.LENGTH_SHORT).show();
             }
+            else {
+                alarmManager.setExact(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent);
 
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    alarmManager.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent);
+                }
+                Toast.makeText(getApplicationContext(),"알림이 한번만 울립니다.",Toast.LENGTH_SHORT).show();
+            }
             // 부팅 후 실행되는 리시버 사용가능하게 설정
             pm.setComponentEnabledSetting(receiver,
                     PackageManager.COMPONENT_ENABLED_STATE_ENABLED,
                     PackageManager.DONT_KILL_APP);
 
         }
-//        else { //Disable Daily Notifications
-//            if (PendingIntent.getBroadcast(this, 0, alarmIntent, 0) != null && alarmManager != null) {
-//                alarmManager.cancel(pendingIntent);
-//                //Toast.makeText(this,"Notifications were disabled",Toast.LENGTH_SHORT).show();
-//            }
-//            pm.setComponentEnabledSetting(receiver,
-//                    PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
-//                    PackageManager.DONT_KILL_APP);
-//        }
+        else { //Disable Daily Notifications
+            if (PendingIntent.getBroadcast(this, 0, alarmIntent, 0) != null && alarmManager != null) {
+                alarmManager.cancel(pendingIntent);
+                Toast.makeText(this,"Notifications were disabled",Toast.LENGTH_SHORT).show();
+            }
+            pm.setComponentEnabledSetting(receiver,
+                    PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
+                    PackageManager.DONT_KILL_APP);
+        }
     }
 }
