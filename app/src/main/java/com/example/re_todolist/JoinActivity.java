f04cbe17a26a3dba03ee04479e5c6f1d6f2e5c25
yegyu-han <yegyuhan@gmail.com>
2022-03-09 10:08:45 +0900

    fix error(login after register)

--- app/src/main/java/com/example/re_todolist/JoinActivity.java
+++ app/src/main/java/com/example/re_todolist/JoinActivity.java
@@ -209,6 +209,7 @@ public class JoinActivity extends AppCompatActivity {
 
                     Toast.makeText(getApplicationContext(), "가입되었습니다.",
                             Toast.LENGTH_LONG).show();
+                    mAuth.signOut();
                     Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
                     startActivity(intent);
                 } else {
